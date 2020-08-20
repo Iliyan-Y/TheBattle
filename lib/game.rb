857cd27544404f9c93816c1c76bc5473c2bb85ea
Iliyan-Y <maximizebg@gmail.com>
2020-08-20 14:06:55 +0300

    Game takes number of players now. Controller has bean refactured

--- lib/game.rb
+++ lib/game.rb
@@ -1,7 +1,21 @@
 class Game
 
+  attr_reader :players
+
+  def initialize(player1, player2)
+    @players = [player1,player2]
+  end
+
   def attack(target)
     target.get_damage
+  end
+
+  def player1
+    @players[0] 
+  end
+
+  def player2
+    @players[1]
   end
   
 end
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,10 +1,18 @@
 require "game"
 
 describe Game do
-  subject(:game) { described_class.new }
   let(:player2) { double("Player", hp:100) }
+  subject(:game) { described_class.new("player1",player2 ) }
+  
 
-  describe "#Attack" do 
+  describe "#players" do
+    it "Check if the gaim has more then 1 player" do 
+      game = Game.new("Player1", "Player2")
+      expect(game.players.length).to be > 1
+    end 
+  end
+
+  describe "#attack" do 
     it "attacks the player and reduces HP by 10" do
       expect(player2).to receive(:get_damage)
       game.attack(player2)
